0afa61c3de688ee015e0847aad54e3c1d260c7c7
Rhys van der Waerden <rhys.vdw@gmail.com>
2016-11-04 12:21:20 +1100

    Add ESLint and StandardJS
    Conform to StandardJS style

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-var exec = require('child_process').exec;
-var path = require('path');
-var uuid = require('node-uuid');
+var exec = require('child_process').exec
+var path = require('path')
+var uuid = require('node-uuid')
 
-module.exports = function(source, map) {
+module.exports = function (source, map) {
+  var callback = this.async()
 
-  var callback = this.async();
-
-  var ioDelimiter = uuid.v4();
+  var ioDelimiter = uuid.v4()
   var child = exec(
     './bin/rails runner ' + path.join(__dirname, 'erb_transformer.rb') + ' ' + ioDelimiter,
-    function(error, stdout) {
-      var sourceRegex = new RegExp(ioDelimiter + '([\\s\\S]+)' + ioDelimiter);
-      var matches = stdout.match(sourceRegex);
-      var transformedSource = matches && matches[1];
-      callback(error, transformedSource, map);
+    function (error, stdout) {
+      var sourceRegex = new RegExp(ioDelimiter + '([\\s\\S]+)' + ioDelimiter)
+      var matches = stdout.match(sourceRegex)
+      var transformedSource = matches && matches[1]
+      callback(error, transformedSource, map)
     }
-  );
+  )
 
-  child.stdin.write(source);
-  child.stdin.end();
+  child.stdin.write(source)
+  child.stdin.end()
 }
