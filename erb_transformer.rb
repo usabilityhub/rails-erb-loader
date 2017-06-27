00747234d5a757756ec726c7538ab533a46bc176
sonoda <raleron7@gmail.com>
2017-06-27 16:40:30 +0900

    rubocop

--- erb_transformer.rb
+++ erb_transformer.rb
@@ -1,17 +1,17 @@
 delimiter = ARGV[0]
 engine = ARGV[1]
 handler = case engine
-  when 'erubi'
-    require 'erubi'
-    Erubi::Engine
-  when 'erubis'
-    require 'erubis'
-    Erubis::Eruby
-  when 'erb'
-    require 'erb'
-    ERB
-  else raise "Unknown templating engine `#{engine}`"
-end
+          when 'erubi'
+            require 'erubi'
+            Erubi::Engine
+          when 'erubis'
+            require 'erubis'
+            Erubis::Eruby
+          when 'erb'
+            require 'erb'
+            ERB
+          else raise "Unknown templating engine `#{engine}`"
+          end
 
 if engine == 'erubi'
   puts "#{delimiter}#{eval(handler.new(STDIN.read).src)}#{delimiter}"
